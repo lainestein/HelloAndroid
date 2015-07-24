64ed74ccafc8373bbefd0ea98f8078981f91ff41
Administrator <laine163@163.com>
2015-07-24 15:33:58 +0800

    删除不需要的菜单部分的代码

--- src/com/laine/helloAndroid/MainActivity.java
+++ src/com/laine/helloAndroid/MainActivity.java
@@ -7,6 +7,9 @@ public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    //this is a comment
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
